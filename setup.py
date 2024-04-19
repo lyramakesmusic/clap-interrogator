dd54092b8110a5bdd0f0451e0a654a5aef65a4ce
Lyra C <lyramakesmusic@gmail.com>
2024-04-19 13:42:37 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="clap_interrogator",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "transformers>=4.40.0"
+    ],
+)
